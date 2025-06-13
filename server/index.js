e595957ed65881ef6cbde6702594ff655e874870
adityasriv2317 <adityaxia9237@gmail.com>
2025-06-13 13:32:31 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("online!!");
+});
+
+const start = () => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+start();
